e832c59a8edc5d68ba90f85ca3638f8c6a58222b
upendra <upendrarajput9911@gmail.com>
2022-03-08 02:01:12 +0530

    block-BNaaag

--- block-BNaaag/index.js
+++ block-BNaaag/index.js
@@ -17,10 +17,12 @@
 // 5. write content(Welcome to Buffer) to buff1 variable.
 var path = require('path')
 var filePath = path.join(__dirname,'content.md')
-var fs = require('fs')
-fs.readFile('./content.md', (err, content) => {
+var {readFile,readFileSync} = require('fs')
+readFile('./content.md', (err, content) => {
 console.log( err, content.toString())
 })
+var result = readFileSync('./content.md','utf8')
+console.log(result)
 var buff1 = Buffer.alloc(10);
 console.log(buff1)
 buff1.write('Welcome to Buffer')
