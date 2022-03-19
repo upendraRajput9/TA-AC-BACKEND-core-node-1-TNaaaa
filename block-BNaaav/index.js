58119dd32e66f7131d21210b23633233552fb973
upendra <upendrarajput9911@gmail.com>
2022-03-19 16:41:31 +0530

    done

--- block-BNaaav/index.js
+++ block-BNaaav/index.js
@@ -7,17 +7,26 @@ var server = http.createServer(handleRequest)
 function handleRequest(req,res){
   let parseURL = url.parse(req.url);
   let pathname = parseURL.pathname;
-  console.log(req.url.split('.').pop(),'pop')
+  // console.log(req.url.split('.').pop(),'pop')
   if(req.method==='GET' && req.url==='/'){
 fs.createReadStream('./index.html').pipe(res)
   }else if(req.method==='GET' && req.url==='/about'){
-fs.createReadStream('./about.html').pipe(res)
+fs.createReadStream('about.html').pipe(res)
   }
     else if(req.url.split('.').pop() === 'css') {
         // set header for css file
         res.setHeader('Content-Type', 'text/css');
         // read css file and send it in response
-        fs.readFile('./assets/stylesheets/' + req.url, (err, content) => {
+        fs.readFile('./' + req.url, (err, content) => {
+          if(err) return console.log(err);
+          res.end(content)
+        })
+      }
+      else if(req.url.split('.').pop() === 'jpg') {
+        // set header for css file
+        res.setHeader('Content-Type', 'image/jpg');
+        // read css file and send it in response
+        fs.readFile('./' + req.url, (err, content) => {
           if(err) return console.log(err);
           res.end(content)
         })
